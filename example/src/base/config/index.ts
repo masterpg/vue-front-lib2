eec090c9b45d046b3af781f0e548e1162df221be
masterpg <masterpg@livedeveloper.net>
2019-11-01 13:54:11 +0900

    ライブラリ開発のサポートスクリプトを改善

--- example/src/base/config/index.ts
+++ example/src/base/config/index.ts
@@ -10,13 +10,13 @@ export function initConfig(): void {
 class AppConfig extends BaseConfig {
   constructor() {
     super({
-      apiKey: 'AIzaSyCy7fBdZ124bZq7flc8pS7c9deBozp_rfs',
-      authDomain: 'vue-base-project-7295.firebaseapp.com',
-      databaseURL: 'https://vue-base-project-7295.firebaseio.com',
-      projectId: 'vue-base-project-7295',
-      storageBucket: 'vue-base-project-7295.appspot.com',
-      messagingSenderId: '1046408958180',
-      appId: '1:1046408958180:web:d83d34b29a37de42',
+      apiKey: '<API_KEY>',
+      authDomain: '<PROJECT_ID>.firebaseapp.com',
+      databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
+      projectId: '<PROJECT_ID>',
+      storageBucket: '<BUCKET>.appspot.com',
+      messagingSenderId: '<SENDER_ID>',
+      appId: '<APP_ID>',
     })
   }
 }
